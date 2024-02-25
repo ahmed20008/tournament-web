ef3bb2d7e9ffd39b41ab5456f3299aaa52e244e8
Mohid <mohidahmed555@gmail.com>
2024-02-25 17:14:30 +0500

    Hotfix update scores

--- src/app/(authenticated)/add-scores/page.jsx
+++ src/app/(authenticated)/add-scores/page.jsx
@@ -3,23 +3,20 @@ import React, { useState, useEffect } from 'react';
 import formStyles from "@/assets/css/form-elements.module.css";
 import styles from "@/assets/css/add-student.module.css";
 import buttonStyles from "@/assets/css/buttons.module.css?v1.1";
-import { collection, getDocs, updateDoc, doc, query, where } from "firebase/firestore";
+import { collection, getDoc, getDocs, updateDoc, doc, query, where } from "firebase/firestore";
 import { db } from '@/config';
 import CircularProgress from '@/components/CircularProgress';
 import { enqueueSnackbar } from "notistack";
 import { calculateAverage } from '@/utils/helperMethod';
 
 const Page = () => {
-  const initialState = {
-    name: '',
-    id: '',
-    class: '',
-  }
+
   const [loading, setLoading] = useState(false);
   const [students, setStudents] = useState([]);
   const [addStudent, setAddStudent] = useState({ name: '' });
   const [selectedSport, setSelectedSport] = useState('');
-  const [sportValues, setSportValues] = useState([]);
+  const [scoreValues, setScoreValues] = useState([]);
+  const [timeValues, setTimeValues] = useState([]);
 
   useEffect(() => {
     const fetchStudents = async () => {
@@ -37,7 +34,8 @@ const Page = () => {
   const handleStudentChange = (e) => {
     setAddStudent({ name: e.target.value });
     setSelectedSport('');
-    setSportValues([]);
+    setScoreValues([]);
+    setTimeValues([]);
   };
 
   const handleSportChange = (sport) => {
@@ -45,16 +43,33 @@ const Page = () => {
 
     const selectedStudent = students.find(student => student.name === addStudent.name);
     if (selectedStudent && selectedStudent.scores && selectedStudent.scores[sport]) {
-      setSportValues([...selectedStudent.scores[sport]]);
-    } else {
-      setSportValues(['', '', '', '']);
+      const sportScores = selectedStudent.scores[sport];
+      const newScoreValues = Array(4).fill('');
+      let newTimeValues = [];
+      if (sport === 'run' || sport === 'setUp') {
+        newTimeValues = Array(4).fill('');
+      }
+      sportScores.forEach((entry, index) => {
+        newScoreValues[index] = entry.score;
+        if (sport === 'run' || sport === 'setUp') {
+          newTimeValues[index] = entry.time;
+        }
+      });
+      setScoreValues(newScoreValues);
+      setTimeValues(newTimeValues);
     }
   };
 
-  const handleSportValueChange = (index, value) => {
-    const newValues = [...sportValues];
+  const handleScoreChange = (index, value) => {
+    const newValues = [...scoreValues];
     newValues[index] = value;
-    setSportValues(newValues);
+    setScoreValues(newValues);
+  };
+
+  const handleTimeChange = (index, value) => {
+    const newValues = [...timeValues];
+    newValues[index] = value;
+    setTimeValues(newValues);
   };
 
   const handleSubmit = async (e) => {
@@ -62,18 +77,26 @@ const Page = () => {
     setLoading(true);
     try {
       const selectedStudent = students.find(student => student.name === addStudent.name);
-      console.log(selectedStudent)
       if (!selectedStudent) {
         throw new Error('Selected student not found.');
       }
       const studentRef = doc(db, 'students', selectedStudent.id);
-      const updatedScores = { ...selectedStudent.scores };
-      updatedScores[selectedSport] = [...sportValues];
-      await updateDoc(studentRef, { scores: updatedScores });
-      enqueueSnackbar('Scores updated successfully!', { variant: "success" })
+
+      const updatedScores = {
+        [selectedSport]: scoreValues.map((score, index) => {
+          const time = (selectedSport === 'run' || selectedSport === 'setUp') ? timeValues[index] : undefined;
+          return { score, ...(time !== undefined && { time }) }; // Include time if it's defined
+        })
+      };
+
+      await updateDoc(studentRef, { [`scores.${selectedSport}`]: updatedScores[selectedSport] });
+      const updatedDocSnapshot = await getDoc(studentRef);
+      const updatedStudent = { id: updatedDocSnapshot.id, ...updatedDocSnapshot.data() };
+      setStudents(students.map(student => student.id === updatedStudent.id ? updatedStudent : student));
+
+      enqueueSnackbar('Scores updated successfully!', { variant: "success" });
     } catch (error) {
-      enqueueSnackbar(`${error}`, { variant: "error" })
-      setLoading(false);
+      enqueueSnackbar(`${error}`, { variant: "error" });
     } finally {
       setLoading(false);
     }
@@ -109,8 +132,8 @@ const Page = () => {
                   onChange={(e) => handleSportChange(e.target.value)}
                 >
                   <option value="">Select Sport</option>
-                  <option value="jumpPlace">Jump from Place</option>
                   <option value="jumpHeight">Jump from Height</option>
+                  <option value="jumpPlace">Jump from Place</option>
                   <option value="run">Run</option>
                   <option value="setUp">Set-up Workout</option>
                 </select>
@@ -118,7 +141,7 @@ const Page = () => {
               {selectedSport && (
                 <div className='mb-3 mx-4'>
                   {/* <h5 className={`${styles.sportHeading}`}>{selectedSport}</h5> */}
-                  {sportValues.map((value, index) => (
+                  {scoreValues.map((value, index) => (
                     <>
                       <label htmlFor="scores" className="form-label px-1">
                         <b>Score {index + 1}</b>
@@ -128,19 +151,38 @@ const Page = () => {
                         type="number"
                         value={value}
                         className={`${formStyles.inputFieldWhite} mb-3`}
-                        onChange={(e) => handleSportValueChange(index, e.target.value)}
+                        onChange={(e) => handleScoreChange(index, e.target.value)}
                         required={true}
                         min={0}
                         max={100}
                       />
                     </>
                   ))}
-                  <p>Automatic Grade: {calculateAverage(sportValues)}</p>
+                  {(selectedSport === 'run' || selectedSport === 'setUp') && (
+                    timeValues.map((value, index) => (
+                      <>
+                        <label htmlFor="distances" className="form-label px-1">
+                          <b>Distance {index + 1}</b>
+                        </label>
+                        <input
+                          key={index}
+                          type="number"
+                          value={value}
+                          className={`${formStyles.inputFieldWhite} mb-3`}
+                          onChange={(e) => handleTimeChange(index, e.target.value)}
+                          required={true}
+                          min={0}
+                          max={100}
+                        />
+                      </>
+                    ))
+                  )}
+                  <p>Automatic Grade: {calculateAverage(scoreValues)}</p>
                 </div>
               )}
               <div className="mt-3 mb-3 mx-4">
                 <button type="submit" className={`${buttonStyles.buttonDarkPink} `}>
-                  {!loading ? "Add Record" : <CircularProgress width={22} height={22} />}
+                  {!loading ? "Update Record" : <CircularProgress width={22} height={22} />}
                 </button>
               </div>
             </form>
