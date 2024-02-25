677e2b20b32441114a9a9fdaf68328086410e1e9
Mohid <mohidahmed555@gmail.com>
2024-02-25 23:39:23 +0500

    Grapgh added

--- src/app/(authenticated)/dashboard/[uuid]/page.jsx
+++ src/app/(authenticated)/dashboard/[uuid]/page.jsx
@@ -8,6 +8,8 @@ import Skeleton from "react-loading-skeleton";
 import Link from 'next/link';
 import { ArrowHeadIcon } from '@/components/IconSVG';
 import { useParams } from 'next/navigation';
+import { Chart as ChartJS } from 'chart.js/auto';
+import { Line } from 'react-chartjs-2';
 
 const Page = () => {
   const params = useParams();
@@ -52,6 +54,54 @@ const Page = () => {
           <p className="px-1 m-0">Go Back</p>
         </Link>
         <h2 className={`${styles.studentDetailMainHeading}`}>Student Score Details</h2>
+        <div>
+          {student && (
+            <Line
+              data={{
+                labels: student.scores.jumpPlace.map((data, index) => `Attempt ${index + 1}`),
+                datasets: [
+                  {
+                    label: "Jump from Place",
+                    data: student.scores.jumpPlace.map((data) => data.score),
+                    backgroundColor: "#064FF0",
+                    borderColor: "#064FF0",
+                  },
+                  {
+                    label: "Jump from Height",
+                    data: student.scores.jumpHeight.map((data) => data.score),
+                    backgroundColor: "#FF3030",
+                    borderColor: "#FF3030",
+                  },
+                  {
+                    label: "Run",
+                    data: student.scores.run.map((data) => data.score),
+                    backgroundColor: "#00FF00",
+                    borderColor: "#00FF00",
+                  },
+                  {
+                    label: "Set-up Workout",
+                    data: student.scores.setUp.map((data) => data.score),
+                    backgroundColor: "#FFFF00",
+                    borderColor: "#FFFF00",
+                  },
+                ],
+              }}
+              options={{
+                elements: {
+                  line: {
+                    tension: 0.5,
+                  },
+                },
+                plugins: {
+                  title: {
+                    text: "Student Scores",
+                  },
+                },
+              }}
+            />
+          )}
+        </div>
+
         <div className="row">
           <h3>{!fetchingData ? student?.name : <Skeleton width={120} />}</h3>
           <p>Class: {!fetchingData ? student?.class : <Skeleton width={80} />}</p>
@@ -83,7 +133,10 @@ const Page = () => {
             <h4 className={`${styles.studentDetailHeading}`}>Run</h4>
             {!fetchingData ? (
               student?.scores.run.map((score, index) => (
-                <p key={index}>Score {index + 1}: <span className='fw-bold'>{score.score ? score.score : 'N/A'}</span></p>
+                <>
+                  <p key={index}>Score {index + 1}: <span className='fw-bold'>{score.score ? score.score : 'N/A'}</span></p>
+                  <p key={index}>Time {index + 1}: <span className='fw-bold'>{score.time ? score.time : 'N/A'}</span></p>
+                </>
               ))
             ) : (
               Array.from({ length: 4 }).map((_, index) => (
@@ -95,7 +148,10 @@ const Page = () => {
             <h4 className={`${styles.studentDetailHeading}`}>Set-up Workout</h4>
             {!fetchingData ? (
               student?.scores.setUp.map((score, index) => (
-                <p key={index}>Score {index + 1}: <span className='fw-bold'>{score.score ? score.score : 'N/A'}</span></p>
+                <>
+                  <p key={index}>Score {index + 1}: <span className='fw-bold'>{score.score ? score.score : 'N/A'}</span></p>
+                  <p key={index}>Time {index + 1}: <span className='fw-bold'>{score.time ? score.time : 'N/A'}</span></p>
+                </>
               ))
             ) : (
               Array.from({ length: 4 }).map((_, index) => (
